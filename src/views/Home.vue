f75bcc67ab440e2a1239a92b6bc8d8844a1b9619
unkelhoebbi <gregi.stuber@gmail.com>
2022-01-27 23:04:30 +0100

    Add statistics.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,40 @@
       ></Semester>
     </div>
   </div>
+  <article>
+    <H2>Übersicht Kategorien/Credits</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="p-2">Name</th>
+          <th class="p-2">Mögliche Credits</th>
+          <th class="p-2">Geplante Credits</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="category in allCategories" :key="category.name">
+          <td class="p-2">
+            {{ category.name }}
+          </td>
+          <td class="p-2">{{ category.possibleCredits }}</td>
+          <td class="p-2">{{ category.earnedCredits }}</td>
+        </tr>
+        <tr>
+          <td class="p-2">Total geplante</td>
+          <td></td>
+          <td class="p-2">{{ totalPlanned }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </article>
 </template>
 
 <script>
 import Semester from '../components/Semester.vue';
 
-const URL = 'https://raw.githubusercontent.com/jeremystucki/ost-planer/main/data/modules.json';
+const BASE_URL = 'https://raw.githubusercontent.com/jeremystucki/ost-planer/main/data';
+const ROUTE_MODULES = '/modules.json';
+const ROUTE_CATEGORIES = '/categories.json';
 export default {
   name: 'Home',
   data() {
@@ -497,12 +525,14 @@ export default {
         },
       ],
       allModules: null,
+      allCategories: null,
+      totalPlanned: 0,
     };
   },
   components: { Semester },
   methods: {
     getAllModules() {
-      fetch(URL).then((response) => {
+      fetch(`${BASE_URL}${ROUTE_MODULES}`).then((response) => {
         if (response.ok) {
           response.json().then((modules) => {
             this.allModules = modules;
@@ -510,9 +540,47 @@ export default {
         }
       });
     },
+    getCategories() {
+      fetch(`${BASE_URL}${ROUTE_CATEGORIES}`).then((response) => {
+        if (response.ok) {
+          response.json().then((categories) => {
+            categories.forEach((category) => {
+              // eslint-disable-next-line no-param-reassign
+              category.possibleCredits = 0;
+              // eslint-disable-next-line no-param-reassign
+              category.earnedCredits = 0;
+              this.allModules.forEach((module) => {
+                if (module.categories.includes(category.name)) {
+                  // eslint-disable-next-line no-param-reassign
+                  category.possibleCredits += module.ects;
+                }
+              });
+              this.semesters.forEach((semester) => {
+                semester.modules.forEach((module) => {
+                  if (module.categories.includes(category.name)) {
+                    // eslint-disable-next-line no-param-reassign
+                    category.earnedCredits += module.ects;
+                  }
+                });
+              });
+            });
+            this.allCategories = categories;
+          });
+        }
+      });
+    },
+    updateTotalPlanned() {
+      this.semesters.forEach((semester) => {
+        semester.modules.forEach((module) => {
+          this.totalPlanned += module.ects;
+        });
+      });
+    },
   },
   mounted() {
     this.getAllModules();
+    this.getCategories();
+    this.updateTotalPlanned();
   },
 };
 </script>
