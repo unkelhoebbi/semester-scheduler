a535c23d55c10591e39775921e1e4bc52059fa2f
Jeremy Stucki <dev@jeremystucki.ch>
2022-02-20 22:14:50 +0100

    Add `Vollzeit` Musterpläne

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -41,8 +41,37 @@
               Data Engineering and Machine Intelligence
             </a>
           </div>
+          <!-- eslint-enable -->
         </div>
-        <!-- eslint-enable -->
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Musterpläne Vollzeit
+          </a>
+
+          <!-- eslint-disable -->
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-KommIng2-SEP1_ExEv-SEP2_SEProj_DigBusI-SAI21_WI2-BAI14_PhAI">
+              Rahmen
+            </a>
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-ParProg_SecSoW_Bsys2_AIAp_DatEng_KommIng2-PmQm_CoBau_MsTe_CPl_SEP1_ExEv-CPlA_DSy_SEP2_SEProj_DigBusI-PF_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+              Software Engineering
+            </a>
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SecSoW_Bsys2_AIAp_DatEng_KommIng2-PmQm_WE3_MsTe_MGE_SEP1_ExEv-HCD_DSy_SEP2_SEProj_DigBusI-ComGra_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+              Frontend Engineering
+            </a>
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SecSoW_WsoT_CN2_KommIng2-PmQm_PFSec_CldInf_NISec_SEP1_ExEv-CldOp_HackL_DSy_SEP2_SEProj_DigBusI-CyDef_AppArch_SAI21_WI2-IncResp_BAI14_PhAI">
+              Cybersecurity
+            </a>
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SDW_WsoT_NetAut_CN2_KommIng2-PmQm_CldInf_NISec_SEP1_ExEv-CldOp_SModSim_DSy_SEP2_SEProj_DigBusI-ServDev_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+              Network and Cloud Infrastructure
+            </a>
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_MathFML_DatAna_AIAp_DatEng_KommIng2-PmQm_ML_MsTe_MGE_SEP1_ExEv-DL_DSy_SEP2_SEProj_DigBusI-BlCh_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+              Data Engineering and Machine Intelligence
+            </a>
+          </div>
+          <!-- eslint-enable -->
+        </div>
       </div>
     </div>
   </nav>
