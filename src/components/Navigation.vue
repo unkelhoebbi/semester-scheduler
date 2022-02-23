1504bb201cf753c71700d19db11731f7eeb5c4f3
Jeremy Stucki <dev@jeremystucki.ch>
2022-02-23 14:09:11 +0100

    Fix `Vollzeit` Musterpläne

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -51,22 +51,22 @@
 
           <!-- eslint-disable -->
           <div class="navbar-dropdown">
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-KommIng2-SEP1_ExEv-SEP2_SEProj_DigBusI-SAI21_WI2-BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_KommIng2-WE1_AlgDat_Bsys1_AIFo_SEP1_ExEv-SEP2_SEProj_DigBusI-PmQm_SAI21_WI2-BAI14_PhAI">
               Rahmen
             </a>
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-ParProg_SecSoW_Bsys2_AIAp_DatEng_KommIng2-PmQm_CoBau_MsTe_CPl_SEP1_ExEv-CPlA_DSy_SEP2_SEProj_DigBusI-PF_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_DatEng_KommIng2-WE1_AlgDat_Bsys1_AIFo_MsTe_CPl_SEP1_ExEv-ParProg_SecSoW_Bsys2_AIAp_DSy_SEP2_SEProj_DigBusI-PmQm_CoBau_PF_AppArch_SAI21_WI2-CPlA_CldSol_BAI14_PhAI">
               Software Engineering
             </a>
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SecSoW_Bsys2_AIAp_DatEng_KommIng2-PmQm_WE3_MsTe_MGE_SEP1_ExEv-HCD_DSy_SEP2_SEProj_DigBusI-ComGra_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_DatEng_KommIng2-WE1_AlgDat_Bsys1_AIFo_MsTe_MGE_SEP1_ExEv-WE2_SecSoW_Bsys2_AIAp_DSy_SEP2_SEProj_DigBusI-PmQm_WE3_ComGra_AppArch_SAI21_WI2-HCD_CldSol_BAI14_PhAI">
               Frontend Engineering
             </a>
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SecSoW_WsoT_CN2_KommIng2-PmQm_PFSec_CldInf_NISec_SEP1_ExEv-CldOp_HackL_DSy_SEP2_SEProj_DigBusI-CyDef_AppArch_SAI21_WI2-IncResp_BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_CN2-WE1_AlgDat_Bsys1_AIFo_CldInf_NISec_SEP1_ExEv-WE2_SecSoW_WsoT_DSy_SEP2_SEProj_DigBusI-PmQm_PFSec_CyDef_AppArch_SAI21_WI2-CldOp_HackL_KommIng2_IncResp_BAI14_PhAI">
               Cybersecurity
             </a>
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_SDW_WsoT_NetAut_CN2_KommIng2-PmQm_CldInf_NISec_SEP1_ExEv-CldOp_SModSim_DSy_SEP2_SEProj_DigBusI-ServDev_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_CN2-WE1_AlgDat_Bsys1_AIFo_CldInf_NISec_SEP1_ExEv-WE2_SDW_WsoT_NetAut_DSy_SEP2_SEProj_DigBusI-PmQm_ServDev_AppArch_SAI21_WI2-CldOp_SModSim_KommIng2_CldSol_BAI14_PhAI">
               Network and Cloud Infrastructure
             </a>
-            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg-WE1_AlgDat_Bsys1_AIFo_Dbs1_EnglScience-WE2_MathFML_DatAna_AIAp_DatEng_KommIng2-PmQm_ML_MsTe_MGE_SEP1_ExEv-DL_DSy_SEP2_SEProj_DigBusI-BlCh_AppArch_SAI21_WI2-CldSol_BAI14_PhAI">
+            <a class="navbar-item" href="#/plan/An1I_OOP1_AutPy_CN1_DMI_RheKI_Dbs1_EnglScience-An2I_OOP2_DigCod_CySec_AutoSpr_FunProg_DatEng_KommIng2-WE1_AlgDat_Bsys1_AIFo_MsTe_MGE_SEP1_ExEv-WE2_MathFML_DatAna_AIAp_DSy_SEP2_SEProj_DigBusI-PmQm_ML_AppArch_BlCh_SAI21_WI2-DL_CldSol_BAI14_PhAI">
               Data Engineering and Machine Intelligence
             </a>
           </div>
