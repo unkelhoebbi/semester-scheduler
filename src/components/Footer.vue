c651121498ca584eae4fa391f6b700b700cdd329
Jeremy Stucki <dev@jeremystucki.ch>
2022-07-18 08:16:39 +0200

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,9 @@
     <div class="pt-5 divider"></div>
       <div class="columns">
         <div class="column is-11">
-          <p>© 2022 Jeremy Stucki, Gregor Stuber, Joshua Hürzeler, Mathias Fischler</p>
+          <p>
+            © 2022 Jeremy Stucki, Gregor Stuber, Joshua Hürzeler, Mathias Fischler, Luzia Kündig
+          </p>
         </div>
         <div class="column is-1 ml-auto">
           <a class="mr-4 is-inline-block" href="https://github.com/unkelhoebbi/semester-scheduler">
