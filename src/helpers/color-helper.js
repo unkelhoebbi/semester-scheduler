dbea642af2c47730a0effe908aa79aa97167eeab
Stefanie Jäger <stefijaeger@gmail.com>
2023-01-01 19:53:30 +0100

    Use new json data
    In a previous branch, I switched to using the json supplied by adunis, instead of crawling the html. This new data is now used in this branch.
    A mapping of 2 old module names (FunProg to FP and BAI14 to BAI21) was required. The module names in the url get updated as well. We should be able to remove that mapping after some months.
    We had to introduce a hirarchical priority for the category colors,
    since some modules can belong to multiple categories.
    I also had to update some dependencies to get it running at all.

--- src/helpers/color-helper.js
+++ src/helpers/color-helper.js
@@ -1,3 +1,8 @@
+const CATEGORY_COLOR_PRIORITIES = {
+  Auf: -1,
+  Inf: -1,
+};
+
 const CATEGORY_COLOR_MAP = {
   Auf: '#f368e0',
   MaPh: '#ee5253',
@@ -5,15 +10,21 @@ const CATEGORY_COLOR_MAP = {
   gwr: '#10ac84',
   Inf: '#576574',
   SaBa: '#222f3e',
-  EP: '#55efc4',
+  EP: '#222f3e',
   RA: '#ff9f43',
   Fallback: '#737373',
 };
 
-// eslint-disable-next-line max-len
-const getColorForCategory = (categoryId) => CATEGORY_COLOR_MAP[categoryId] || CATEGORY_COLOR_MAP.Fallback;
+// eslint-disable-next-line vue/max-len
+const getColorForCategoryId = (categoryId) => CATEGORY_COLOR_MAP[categoryId] || CATEGORY_COLOR_MAP.Fallback;
+
+const getCategoryColorForModule = (module) => module.categories
+  .sort((a, b) => CATEGORY_COLOR_PRIORITIES[b.id] ?? 0 - CATEGORY_COLOR_PRIORITIES[a.id] ?? 0)
+  .map((category) => category.id)
+  .map(getColorForCategoryId)[0] ?? CATEGORY_COLOR_MAP.Fallback;
 
 export {
   // eslint-disable-next-line import/prefer-default-export
-  getColorForCategory,
+  getColorForCategoryId,
+  getCategoryColorForModule,
 };
